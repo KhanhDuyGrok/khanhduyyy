67b737f39d55b983f8676f9161b2ef26d1f2eafa
KhanhDuyGrok <khanhduy140188@gmail.com>
2025-11-30 10:03:40 +0700

    Update hehe.lua

--- hehe.lua
+++ hehe.lua
@@ -173,7 +173,6 @@ repeat
 until player.Team
 hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Death), function() end)
 hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Respawn), function() end)
-local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/hluuvn/BloxFruit/refs/heads/main/MainShadow.lua"))()
 Window = Fluent:CreateWindow({
     Title = "Shadow Hub",
     SubTitle="Blox Fruit", 
